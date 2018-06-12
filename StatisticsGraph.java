fded4586eb4869455d157ea3213557feb0fc6800
Tristan Lonsway <tblonswa@syr.edu>
2018-06-12 09:44:19 -0400

    Fixed graph scaling and added coloration

--- StatisticsGraph.java
+++ StatisticsGraph.java
@@ -29,19 +29,32 @@ public class StatisticsGraph extends JComponent{
         System.out.println("min y : " + min_y);
         System.out.println("max y : " + max_y);
         System.out.println("max x : " + max_x);
+        g.setColor(Color.GRAY);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
         if (points.size()>0) {
             double psx = WIDTH*((points.get(0).getX())/max_x);
-            double psy = HEIGHT*((points.get(0).getY())/max_y);
+            double psy = HEIGHT*((points.get(0).getY())/max_y)-min_y;
+            psy-=15;
             for(GraphPoint p : points) {
                 double sx = WIDTH*(p.getX()/max_x);
-                double sy = ((HEIGHT*(p.getY()/max_y))+30)-min_y;
-                //System.out.println("sx: " + sx);
-                //System.out.println("sy: " + sy);
-                //System.out.println("psx: " + psx);
-                //System.out.println("psy: " + psy);
-                g.setColor(Color.RED);
-                g.fillRect((int)((sx)-2), (int)((HEIGHT-sy)-2), 4, 4);
+                //double sy = HEIGHT*((((p.getY()/max_y)))-min_y);
+                double sy = (HEIGHT*(p.getY()/max_y))-(HEIGHT*(min_y/max_y));
+                sy+=30;
+                System.out.println("cy: " + p.getY());
+                System.out.println("sx: " + sx);
+                System.out.println("sy: " + sy);
+                System.out.println("psx: " + psx);
+                System.out.println("psy: " + psy);
                 g.setColor(Color.BLACK);
+                g.fillRect((int)((sx)-2), (int)((HEIGHT-sy)-2), 4, 4);
+                //g.setColor(Color.BLACK);
+                if (sy>psy) {
+                    g.setColor(Color.GREEN);
+                } else if (psy>sy) {
+                    g.setColor(Color.RED);
+                } else {
+                    g.setColor(Color.BLACK);
+                }
                 g.drawLine((int)sx, (int)(HEIGHT-sy), (int)psx, (int)(HEIGHT-psy));
                 psx=sx;
                 psy=sy;
@@ -64,16 +77,16 @@ public class StatisticsGraph extends JComponent{
         GraphPoint gp = new GraphPoint(_x_,_y_);
         points.add(gp);
         if (points.size()==1) {
-            min_y=(.8*gp.getY());
+            min_y=(.9*gp.getY());
         }
-        if (gp.getX()>max_x) {
-            max_x=(1.2*(gp.getX()));
+        if (gp.getX()>(max_x/1.1)) {
+            max_x=(1.1*(gp.getX()));
         }
-        if (gp.getY()>max_y) {
-            max_y=(1.2*(gp.getY()));
+        if (gp.getY()>(max_y/1.1)) {
+            max_y=(1.1*(gp.getY()));
         }
-        if (gp.getY()<(min_y/.8)) {
-            min_y=(.8*gp.getY());
+        if (gp.getY()<(min_y/.9)) {   
+            min_y=(.9*gp.getY());
         }
     }
 }
