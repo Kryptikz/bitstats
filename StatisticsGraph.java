38bb59318dc442a405767b3e5f6172fc74e45caf
Tristan Lonsway <tblonswa@syr.edu>
2018-06-13 15:30:12 -0400

    Added in place value reading, where the value for the closest point to the mouse is listed on the screen

--- StatisticsGraph.java
+++ StatisticsGraph.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.util.*;
+import javafx.geometry.BoundingBox;
 public class StatisticsGraph extends JComponent{
     ArrayList<GraphPoint> points;
     double max_x;
@@ -9,6 +10,10 @@ public class StatisticsGraph extends JComponent{
     double min_y;
     int WIDTH=1900;
     int HEIGHT=1050;
+    MouseThread mt;
+    double value;
+    GraphPoint closest;
+    GraphPoint mouse;
     public StatisticsGraph() {
         super();
         points = new ArrayList<GraphPoint>();
@@ -16,120 +21,138 @@ public class StatisticsGraph extends JComponent{
         max_y=5;
         min_x=0;
         min_y=0;
+        value=0;
+        mt = new MouseThread(this);
+        (new Thread(mt)).start();
     }
     public void redraw() {
         repaint();
     }
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.RED);
-        //System.out.println("points size: " + points.size());
-        //System.out.println("max x: " + max_x);
-        //System.out.println("max y: " + max_y);
-        System.out.println("min y : " + min_y);
-        System.out.println("max y : " + max_y);
-        //System.out.println("max x : " + max_x);
-        g.setColor(Color.GRAY);
-        g.fillRect(0, 0, WIDTH, HEIGHT);
-        double av=0;
-        for(GraphPoint p : points) {
-            av+=p.getY();
-        }
-        av/=(double)points.size();
-        g.setColor(Color.BLACK);
-        g.drawString("MEAN: " + av,30,20);
-        double avy = (HEIGHT*((av-min_y)/(max_y-min_y)));
-        g.setColor(Color.ORANGE);
-        g.drawLine(0, (int)avy, WIDTH, (int)avy);
-        g.setColor(Color.BLACK);
-        ArrayList<GraphPoint> medianlist = new ArrayList<GraphPoint>();
-        for(GraphPoint p : points) {
-            medianlist.add(p);
-        }
-        Collections.sort(medianlist);
-        g.drawString("MEDIAN: " + (medianlist.get((int)(medianlist.size()/2))).getY(),30,40);
-        double stdev=0;
-        for(GraphPoint p : points) {
-            stdev+=(p.getY()-av)*(p.getY()-av);
-        }
-        stdev/=(double)points.size();
-        stdev=Math.sqrt(stdev);
-        g.drawString("STANDARD DEVIATION: " + stdev,30,60);      
-        //double maxy = (HEIGHT*((max_y-min_y)/(max_y-min_y)));
-        g.setColor(Color.BLUE);
-        g.drawLine(0,(int)(HEIGHT-(HEIGHT/1.1))-30,WIDTH,(int)(HEIGHT-(HEIGHT/1.1))-30);
-        g.drawLine(0,(int)(HEIGHT-((HEIGHT)/1.1/2))-30,WIDTH,(int)(HEIGHT-((HEIGHT)/1.1/2))-30);
-        g.drawLine(0,(int)(HEIGHT-((HEIGHT)/.9)-30),WIDTH,(int)(HEIGHT-((HEIGHT)/.9)-30));
-        
-        g.setColor(Color.MAGENTA);
-        double medy = (HEIGHT*((((medianlist.get((int)(medianlist.size()/2))).getY())-min_y)/(max_y-min_y)));
-        g.drawLine(0,(int)medy,WIDTH,(int)medy);
-        
-        double[] xlist = new double[points.size()];
-        double[] ylist = new double[points.size()];
-        for(int i=0;i<points.size();i++) {
-            GraphPoint p = points.get(i);
-            xlist[i]=p.getX();
-            ylist[i]=p.getY();
-        }
-        LinearEquation ln = Calculate.LinearRegression(xlist,ylist);
-        double ln_y1 = ln.calc(0);
-        double ln_y2 = ln.calc((max_x));
-        g.setColor(Color.PINK);
-        double ln_y1_scale = (HEIGHT*((ln_y1-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
-        double ln_y2_scale = (HEIGHT*((ln_y2-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
-        ln_y1_scale+=30;
-        ln_y2_scale+=30;
-        System.out.println("ln_y1 : " + ln_y1_scale)  ;
-        System.out.println("ln_y2 : " + ln_y2_scale);
-        g.drawLine(0,HEIGHT-(int)ln_y1_scale,(int)(WIDTH),HEIGHT-(int)ln_y2_scale);        
-        if (points.size()>0) {
-            double psx = WIDTH*((points.get(0).getX())/max_x);
-            double psy = (HEIGHT*((points.get(0).getY()-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
-            psy+=30;
+        try {
+            super.paintComponent(g);
+            g.setColor(Color.RED);
+            //System.out.println("points size: " + points.size());
+            //System.out.println("max x: " + max_x);
+            //System.out.println("max y: " + max_y);
+            //System.out.println("min y : " + min_y);
+            //System.out.println("max y : " + max_y);
+            //System.out.println("max x : " + max_x);
+            g.setColor(Color.GRAY);
+            g.fillRect(0, 0, WIDTH, HEIGHT);
+            g.setColor(Color.BLACK);
+            g.drawString("VALUE: " + value,WIDTH-200,30);
+            double av=0;
             for(GraphPoint p : points) {
-                double sx = WIDTH*(p.getX()/max_x);
-                //double sy = HEIGHT*((((p.getY()/max_y)))-min_y);
-                //double sy = (HEIGHT*(p.getY()/max_y))-(HEIGHT*(min_y/max_y));
-                //double sy = (HEIGHT*(p.getY()/max_y));
-                double sy = (HEIGHT*((p.getY()-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
-                sy+=30;
-                //System.out.println("cy: " + p.getY());
-                //System.out.println("sx: " + sx);
-                //System.out.println("sy: " + sy);
-                //System.out.println("psx: " + psx);
-                //System.out.println("psy: " + psy);
-                g.setColor(Color.BLACK);
-                g.fillRect((int)((sx)-2), (int)((HEIGHT-sy)-2), 4, 4);
-                //g.setColor(Color.BLACK);
-                if (sy>psy) {
-                    g.setColor(Color.GREEN);
-                } else if (psy>sy) {
-                    g.setColor(Color.RED);
-                } else {
-                    g.setColor(Color.BLACK);
-                }
-                g.drawLine((int)sx, (int)(HEIGHT-sy), (int)psx, (int)(HEIGHT-psy));
-                psx=sx;
-                psy=sy;
+                av+=p.getY();
             }
+            av/=(double)points.size();
+            av=((double)((int)(av*1000)))/1000;
+            g.setColor(Color.BLACK);
+            g.drawString("MEAN: " + av,30,20);
+            double avy = (HEIGHT*((av-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+            avy+=30;
+            g.setColor(Color.ORANGE);
+            g.drawLine(0, HEIGHT-(int)avy, WIDTH, HEIGHT-(int)avy);
+            g.setColor(Color.BLACK);
+            ArrayList<GraphPoint> medianlist = new ArrayList<GraphPoint>();
+            for(GraphPoint p : points) {
+                medianlist.add(p);
+            }
+            Collections.sort(medianlist);
+            double median = 0;
+            if (medianlist.size()%2==0) {
+                median = (medianlist.get((int)(medianlist.size()/2))).getY();    
+            } else {
+                median = (((medianlist.get((int)(medianlist.size()/2))).getY())+((medianlist.get(((int)((medianlist.size()/2)-1)))).getY()))/2;
+            }
+            g.drawString("MEDIAN: " + median,30,40);
+            double stdev=0;
+            for(GraphPoint p : points) {
+                stdev+=(p.getY()-av)*(p.getY()-av);
+            }
+            stdev/=(double)points.size();
+            stdev=Math.sqrt(stdev);
+            stdev=((double)((int)(stdev*1000)))/1000;
+            g.drawString("STANDARD DEVIATION: " + stdev,30,60);      
+            //double maxy = (HEIGHT*((max_y-min_y)/(max_y-min_y)));
+            g.setColor(Color.BLUE);
+            //g.drawLine(0,(int)(HEIGHT-(HEIGHT/1.1))-30,WIDTH,(int)(HEIGHT-(HEIGHT/1.1))-30);
+            //g.drawLine(0,(int)(HEIGHT-((HEIGHT)/1.1/2))-30,WIDTH,(int)(HEIGHT-((HEIGHT)/1.1/2))-30);
+            //g.drawLine(0,(int)(HEIGHT-((HEIGHT)/.9)-30),WIDTH,(int)(HEIGHT-((HEIGHT)/.9)-30));
+            g.setColor(Color.MAGENTA);
+            double medy = (HEIGHT*((((medianlist.get((int)(medianlist.size()/2))).getY())-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+            medy+=30;
+            g.drawLine(0,HEIGHT-(int)medy,WIDTH,HEIGHT-(int)medy);
+            
+            double[] xlist = new double[points.size()];
+            double[] ylist = new double[points.size()];
+            for(int i=0;i<points.size();i++) {
+                GraphPoint p = points.get(i);
+                xlist[i]=p.getX();
+                ylist[i]=p.getY();
+            }
+            LinearEquation ln = Calculate.LinearRegression(xlist,ylist);
+            double ln_y1 = ln.calc(0);
+            double ln_y2 = ln.calc((max_x));
+            g.setColor(Color.PINK);
+            double ln_y1_scale = (HEIGHT*((ln_y1-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+            double ln_y2_scale = (HEIGHT*((ln_y2-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+            ln_y1_scale+=30;
+            ln_y2_scale+=30;
+            //System.out.println("ln_y1 : " + ln_y1_scale)  ;
+            //System.out.println("ln_y2 : " + ln_y2_scale);
+            g.drawLine(0,HEIGHT-(int)ln_y1_scale,(int)(WIDTH),HEIGHT-(int)ln_y2_scale);  
+            
+            g.setColor(new Color(255,255,255,185));
+            double mx = mouse.getX();
+            double my = mouse.getY();
+            double cx = closest.getX();
+            double cy = closest.getY();
+            //cy = 
+            my-=27;
+            cy-=30;
+            g.drawLine((int)mx, (int)my, (int)cx, (int)cy);
+            
+            if (points.size()>0) {
+                double psx = WIDTH*((points.get(0).getX())/max_x);
+                double psy = (HEIGHT*((points.get(0).getY()-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+                psy+=30;
+                for(GraphPoint p : points) {
+                    double sx = WIDTH*(p.getX()/max_x);
+                    //double sy = HEIGHT*((((p.getY()/max_y)))-min_y);
+                    //double sy = (HEIGHT*(p.getY()/max_y))-(HEIGHT*(min_y/max_y));
+                    //double sy = (HEIGHT*(p.getY()/max_y));
+                    double sy = (HEIGHT*((p.getY()-(min_y/.9))/((max_y/1.1)-(min_y/.9))));
+                    sy+=30;
+                    //System.out.println("cy: " + p.getY());
+                    //System.out.println("sx: " + sx);
+                    //System.out.println("sy: " + sy);
+                    //System.out.println("psx: " + psx);
+                    //System.out.println("psy: " + psy);
+    
+                    g.setColor(Color.BLACK);
+                    g.fillRect((int)((sx)-2), (int)((HEIGHT-sy)-2), 4, 4);
+                    //g.setColor(Color.BLACK);
+                    if (sy>psy) {
+                        g.setColor(Color.GREEN);
+                    } else if (psy>sy) {
+                        g.setColor(Color.RED);
+                    } else {
+                        g.setColor(Color.BLACK);
+                    }
+                    g.drawLine((int)sx, (int)(HEIGHT-sy), (int)psx, (int)(HEIGHT-psy));
+                    psx=sx;
+                    psy=sy;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     public void addPoint(GraphPoint gp) {
         points.add(gp);
-        if (gp.getX()>max_x) {
-            max_x=1.2*gp.getX();
-        }
-        if (gp.getY()>max_y) {
-            max_y=1.2*gp.getY();
-        }
-        if (gp.getY()<min_y) {
-            min_y=gp.getY();
-        }
-    }
-    public void addPoint(double _x_, double _y_) {
-        GraphPoint gp = new GraphPoint(_x_,_y_);
-        points.add(gp);
+        mt.addPoint(gp);
         if (points.size()==1) {
             min_y=(.9*gp.getY());
         }
@@ -142,5 +165,47 @@ public class StatisticsGraph extends JComponent{
         if (gp.getY()<(min_y)/.9) {   
             min_y=(.9*gp.getY());
         }
+    }
+    public void addPoint(double _x_, double _y_) {
+        GraphPoint gp = new GraphPoint(_x_,_y_);
+        points.add(gp);
+        mt.addPoint(gp);
+        if (points.size()==1) {
+            min_y=(.9*gp.getY());
+        }
+        if (gp.getX()>(max_x/1.1)) {
+            max_x=(1.1*(gp.getX()));
+        }
+        if (gp.getY()>(max_y/1.1)) {
+            max_y=((1.1*gp.getY()));
+        }
+        if (gp.getY()<(min_y)/.9) {   
+            min_y=(.9*gp.getY());
+        }
+    }
+    public int getWIDTH() {
+        return WIDTH;
+    }
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+    public double getMaxX() {
+        return max_x;
+    }
+    public double getMinY() {
+        return min_y;
+    }
+    public double getMaxY() {
+        return max_y;
+    }
+    public void setValue(double v) {
+        value=v;
+        redraw();
+    }
+    public void setClosest(GraphPoint g) {
+        closest=g;
+    }
+    public void setMouse(GraphPoint g) {
+        mouse=g;
     }
 }
--- initGraph.java
+++ initGraph.java
@@ -10,7 +10,7 @@ public class initGraph {
         StatisticsGraph sg = new StatisticsGraph();
         frame.add(sg);
         sg.setVisible(true);
-        /*for(int i=1;i<1401;i++) {
+        /*for(int i=0;i<1400;i++) {
             double y = webData.getCurrent();
             sg.addPoint(i,y);
             sg.redraw();
@@ -20,8 +20,8 @@ public class initGraph {
                 e.printStackTrace();
             }
         }*/
-        /*for(int i=0;i<1000;i++) {
-            double y = (Math.random()*100)+7500;
+        /*for(int i=0;i<10000;i++) {
+            double y = (Math.random()*100000);
             sg.addPoint(i,y);
             sg.redraw();
             try {
@@ -30,7 +30,8 @@ public class initGraph {
                 e.printStackTrace();
             }
         }*/
-        /*for(int i=0;i<100;i++) {
+        
+        for(int i=0;i<100;i++) {
             sg.addPoint(i,i);
             sg.redraw();
             try {
@@ -39,7 +40,7 @@ public class initGraph {
                 e.printStackTrace();
             }
         }
-        for(int i=100;i<201;i++) {
+        for(int i=100;i<=201;i++) {
             sg.addPoint(i, Math.abs(201-i));
             sg.redraw();
             try {
@@ -47,15 +48,47 @@ public class initGraph {
             } catch (Exception e) { 
                 e.printStackTrace();
             }
-        }*/
-        for(int i=0;i<1000;i++) {
-            sg.addPoint(i,i*i);
+        }
+        /*for(int i=0;i<1000;i++) {
+            sg.addPoint(i,i*i*i);
             sg.redraw();
             try {
                 Thread.sleep(100);
             } catch (Exception e) { 
                 e.printStackTrace();
             }
-        }
+        }*/
+        /*for(int i=0;i<10000;i++) {
+            double x=Math.random()*1000;
+            double y=Math.random()*10000;
+            sg.addPoint(x,y);
+            sg.redraw();
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) { 
+                e.printStackTrace();
+            }
+        }*/
+        /*for(double i=0;i<100;i+=.1) {
+            double y = Math.tan(i)+10;
+            sg.addPoint(i,y);
+            sg.redraw();
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) { 
+                e.printStackTrace();
+            }
+        }*/
+        /*for(int i=-1000;i<1000;i++) {
+            double y = (2*(i*i*i))+(4.3*(i*i))-(1.2*i)+93;
+            sg.addPoint(i,y);
+            sg.redraw();
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) { 
+                e.printStackTrace();
+            }
+        }*/
+        
     }
 }
